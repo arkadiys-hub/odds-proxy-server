6b110db3bff9c928edcc69c531b02de60b0a1de3
arkadiys-hub <ark.kib@mail.ru>
2025-07-29 18:54:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,3 +20,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     # Запускаем на 0.0.0.0, чтобы сервис был доступен извне (Render)
     app.run(host="0.0.0.0", port=port)
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
