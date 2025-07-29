c5151f93ad96202196ee45fd9146574148ceb0d2
arkadiys-hub <ark.kib@mail.ru>
2025-07-29 19:55:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import os
 
 app = Flask(__name__)
-CORS(app)  # Включаем CORS, чтобы разрешить запросы с других источников
+CORS(app)  # Разрешаем CORS, чтобы можно было обращаться с других доменов
 
 API_KEY = "d5f376fc9cd813e26279f6f0bf10d6f6"
 API_URL = f"https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=eu&markets=h2h&apiKey={API_KEY}"
@@ -13,6 +13,7 @@ API_URL = f"https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=eu&mar
 def get_odds():
     try:
         response = requests.get(API_URL)
+        response.raise_for_status()
         data = response.json()
         return jsonify(data)
     except Exception as e:
@@ -21,5 +22,6 @@ def get_odds():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
